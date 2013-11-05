a44776a8652ccfada6de05414eda1a888f6e2648
Britney Wright <blw06g@gmail.com>
2013-11-05 16:29:43 -0500

    update for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Halloween::Application.routes.draw do
-  resources :candies
-
-  resources :kids
+  resources :candies, :kids
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
